040ab938038814bbe98b21b6b077de4cdf3b6a66
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-26 17:33:42 +0600

    init: setup api and client project

---
+++ AuthApi/Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace AuthApi.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string>? Errors { get; set; }
+    }
+}

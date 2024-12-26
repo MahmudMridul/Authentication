8fc22435ff80d611dec78d4771d907295e1a5fdd
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-26 18:01:24 +0600

    config: add db context

---
+++ AuthApi/Db/AuthContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Db
+{
+    public class AuthContext : IdentityDbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "User", NormalizedName = "USER" },
+                new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Manager", NormalizedName = "MANAGER" }
+            );
+        }
+    }
+}

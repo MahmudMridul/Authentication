c8c220cd8a0f4b93dbbdfd493be8a2d558b8a522
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-27 07:24:34 +0600

    update: add Create function to create api response object

--- AuthApi/Models/ApiResponse.cs
+++ AuthApi/Models/ApiResponse.cs
@@ -4,10 +4,22 @@ namespace AuthApi.Models
 {
     public class ApiResponse
     {
-        public object? Data { get; set; }
-        public bool Success { get; set; }
+        public object? Data { get; set; } = null;
+        public bool Success { get; set; } = false;
         public string Message { get; set; } = string.Empty;
         public HttpStatusCode StatusCode { get; set; }
-        public List<string>? Errors { get; set; }
+        public List<string>? Errors { get; set; } = null;   
+
+        public static ApiResponse Create(HttpStatusCode statusCode, object? data = default, bool success = false, string msg = "", List<string>? errors = null)
+        {
+            return new ApiResponse()
+            {
+                Data = data,
+                Success = success,
+                Message = msg,
+                StatusCode = statusCode,
+                Errors = errors
+            };
+        }
     }
 }

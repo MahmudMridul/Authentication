6408e18349ed4b803869c7a6d7e05dfb638c6003
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-27 08:15:27 +0600

    config: modify table names

--- AuthApi/Db/AuthContext.cs
+++ AuthApi/Db/AuthContext.cs
@@ -13,6 +13,15 @@ namespace AuthApi.Db
         {
             base.OnModelCreating(builder);
 
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            
+
             builder.Entity<IdentityRole>().HasData(
                 new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "User", NormalizedName = "USER" },
                 new IdentityRole { Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" },

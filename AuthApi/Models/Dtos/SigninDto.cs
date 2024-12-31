06c553db4dbb0595222254d8cc576b3f1cefd3b1
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-31 16:45:02 +0600

    refactor: property name

--- AuthApi/Models/Dtos/SigninDto.cs
+++ AuthApi/Models/Dtos/SigninDto.cs
@@ -2,7 +2,7 @@
 {
     public class SigninDto
     {
-        public string UserName { get; set; } = string.Empty;
+        public string NameOrEmail { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }

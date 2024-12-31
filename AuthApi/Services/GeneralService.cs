c7a9604f5467a2abb7cf6506b0f8f93f871cd1cc
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-12-31 16:43:51 +0600

    add: IsValidEmail function

---
+++ AuthApi/Services/GeneralService.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthApi.Services
+{
+    public class GeneralService
+    {
+        public static bool IsValidEmail(string str)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(str);
+                return addr.Address == str;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}

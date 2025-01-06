8c3d59c9ca46d1ed21fe7f2b70d837790f77f9f9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-01-06 12:50:12 +0600

    chore: new query

--- AuthDb/authdb.sql
+++ AuthDb/authdb.sql
@@ -7,10 +7,13 @@ select *
 from Users;
 
 select *
-from Re
+from RefreshTokens;
 
-update Users
-set RefreshTokenExpiryTime = '2025-01-01'
-where UserName = 'UserFive@5';
+--update Users
+--set RefreshTokenExpiryTime = '2025-01-01'
+--where UserName = 'UserFive@5';
 
-delete from Users;
+--delete from Users;
+
+select u.UserName, u.Email, rt.Token, rt.Expires
+from Users u inner join RefreshTokens rt on u.Id = rt.UserId;
